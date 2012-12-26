cd41cfe94fb1dd405cb01959e1853f8cd194e859
Mehdi Shams <shams@inanidea.com>
2012-12-26 10:54:31 +0330

    adding bitmap

---
+++ src/visual/Bitmap.js
@@ -0,0 +1,74 @@
+(function (){
+var Bitmap = function(img, left, top, width, height){
+	dream.visual.Bitmap._superClass.call(this, left, top);
+	this._width = width;
+	this._height = height;
+	this.rect.width=width;
+	this.rect.height=height;
+	this.setUp(img);
+}.inherits(dream.visual.Graphic);
+
+var $ = Bitmap.prototype;
+
+$.setUp = function(img){
+	if (typeof img == "string"){
+		this.source = null;
+		var r = new dream.util.ArrayList;
+		r.add(new dream.preload.ImageResource(img));
+		this.requiredResources = r;
+		this.url = img;
+		this.type = "Picture";
+	}
+	else if (img instanceof ImageData){
+		var buff = new dream.util.BufferCanvas(img.width, img.height);
+		this.source = buff.canvas;
+		buff.context.putImageData(img, 0, 0);
+		this.type = "ImageData";
+	}
+	else 
+		console.log("invalid input argument");
+};
+
+Object.defineProperty($, "img", {
+	set : function(v) {
+		this.setUp(v);
+		this.isImageChanged = true;
+	}
+});
+
+Object.defineProperty($, "width", {
+	get : function() {
+		return this._width;
+	},
+	set : function(v) {
+		this._width = v;
+		this.rect.width = v;
+		this.isBoundaryChanged = true;
+		this.isImageChanged = true;
+	}
+});
+Object.defineProperty($, "height", {
+	get : function() {
+		return this._height;
+	},
+	set : function(v) {
+		this._height = v;
+		this.rect.height = v;
+		this.isBoundaryChanged = true;
+		this.isImageChanged = true;	}
+});
+
+$.drawImage = function(ctx, origin){
+	if (! this.source)
+		this.source = dream.preload.cache[this.url].content;
+	ctx.drawImage(this.source, 0, 0, this.source.width, this.source.height, origin.left|0, origin.top|0, this._width, this._height);
+};
+
+
+
+
+
+// exports
+dream.visual.Bitmap = Bitmap;
+
+})();
